88d7b0b856b51828a900478178cc064773aaabbc
Juri Berlanda <juriberlanda@hotmail.com>
2022-06-08 13:35:34 +0200

    [CI] Switch to j-be/rust-diesel-postgres:armv7 based builds.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- docker/armhf-rust-buildenv/files/build_with.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get purge -y libpq-dev:armhf libpq-dev:amd64
-apt-get autoremove -y
-apt-get install -y libpq-dev:$1
-cargo build --target=armv7-unknown-linux-gnueabihf --release
